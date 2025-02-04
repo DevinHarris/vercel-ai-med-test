57a396bc707796f172df38c44b64924204e7dffd
Devin Harris <hello@devinhrrs.me>
2025-02-04 00:41:31 -0500

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { readStreamableValue } from "ai/rsc"
 export const maxDuration = 30;
 
 const fontColor = '#979eaa'
+const fontColor2 = '#5b5f65'
 
 export default function Home() {
   const [conversation, setConversation] = useState<Message[]>([]);
@@ -14,8 +15,8 @@ export default function Home() {
 
   return (
     <div>
-      <div className="w-full text-center py-5" style={{ color: fontColor, borderBlockEnd: `1px solid ${fontColor}`}}>
-         <h1 className="my-5 text-2xl" style={{ fontWeight: '600' }}>AI-Powered Gut Health Solutions</h1>
+      <div className="w-full text-center py-5" style={{ borderBlockEnd: `1px solid #262a30`}}>
+         <h1 className="my-5 text-2xl" style={{ fontWeight: '600', color: fontColor }}>AI-Powered Gut Health Solutions</h1>
          <p>Experience a cutting-edge approach to digestive wellness with AI-driven personalized treatment plans. Our advanced system analyzes symptoms, dietary habits, and health history to craft a natural, science-backed solution tailored to your needs. Using intelligent insights, we recommend targeted dietary adjustments and supplements to restore balance and optimize gut health. Take control of your well-being with precision-guided strategies and long-term relief.</p>
       </div>
       <div style={{ color: fontColor, paddingBlockEnd: '10rem' }}>
@@ -30,12 +31,13 @@ export default function Home() {
         <input 
           type="text"
           placeholder="Start chatting with our AI!"
-          className=" dark:bg-zinc-900  px-10 py-5 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
+          className=" dark:bg-zinc-900  px-10 py-5 border border-zinc-300 dark:border-zinc-800 shadow-xl"
           onChange={event => {
             setInput(event.target.value)
           }} />
         <button
-        className="px-10 py-5 bg-slate-800 text-white"
+        className="px-10 py-5 bg-slate-800"
+        style={{ color: fontColor2 }}
           onClick={async () => {
             const { messages, newMessage } = await continueConversation([
               ...conversation,
