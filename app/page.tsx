0213f7634a3bf88dde5ee1474836107ac5085f66
Devin Harris <hello@devinhrrs.me>
2025-02-04 00:36:10 -0500

    bug fixes

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,11 @@ export default function Home() {
 
   return (
     <div>
-      <div className="w-full text-center py-5" style={{ color: fontColor, borderBlockEnd: `1px solid ${fontColor}` }}>
+      <div className="w-full text-center py-5" style={{ color: fontColor, borderBlockEnd: `1px solid ${fontColor}`}}>
          <h1 className="my-5 text-2xl" style={{ fontWeight: '600' }}>AI-Powered Gut Health Solutions</h1>
          <p>Experience a cutting-edge approach to digestive wellness with AI-driven personalized treatment plans. Our advanced system analyzes symptoms, dietary habits, and health history to craft a natural, science-backed solution tailored to your needs. Using intelligent insights, we recommend targeted dietary adjustments and supplements to restore balance and optimize gut health. Take control of your well-being with precision-guided strategies and long-term relief.</p>
       </div>
-      <div style={{ color: fontColor }}>
+      <div style={{ color: fontColor, paddingBlockEnd: '10rem' }}>
         {conversation.map((message, index) => (
           <div className="my-10" key={index}>
               <span style={{ fontWeight: '500' }}>{message.role}</span>: <p>{message.content}</p>
@@ -26,11 +26,11 @@ export default function Home() {
         ))}
       </div>
 
-      <div className="fixed bottom-0 left-0 right-0 flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      <div className="fixed bottom-0 left-0 right-0 flex flex-col w-full mx-auto">
         <input 
           type="text"
           placeholder="Start chatting with our AI!"
-          className=" dark:bg-zinc-900  px-10 py-5 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
+          className=" dark:bg-zinc-900  px-10 py-5 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
           onChange={event => {
             setInput(event.target.value)
           }} />
