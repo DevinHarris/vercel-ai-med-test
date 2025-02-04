292926baf9654c6e24f912ec918fa175a7d82149
Devin Harris <hello@devinhrrs.me>
2025-02-04 18:25:37 -0500

    add markdown support for chat messages

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
       <div style={{ color: fontColor, paddingBlockEnd: '10rem' }}>
         {conversation.map((message, index) => (
           <div className="my-10" key={index}>
-              <span style={{ fontWeight: '500' }}>{message.role}</span>: <ReactMarkdown children={`${message.content}`} remarkPlugins={[remarkGfm]} />
+              <span style={{ fontWeight: '500' }}>{message.role}</span>: <p>{message.content}</p>
           </div>
         ))}
       </div>
